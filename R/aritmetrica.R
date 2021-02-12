cdb52ba1acc5704d665424429998bf52cb6324ce
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2021-02-11 21:46:56 -0300

    first commit

---
+++ R/aritmetrica.R
@@ -0,0 +1,29 @@
+#' Function to sum
+#'
+#' This function is good to sum.
+#'
+#' @param a A number.
+#' @param b It is another number.
+#'
+#' @examples
+#' sum(2, 3)
+#'
+#' @export
+somar <- function(a, b) {
+  sum(a,b)
+}
+
+#' Function to subtract
+#'
+#' This function is good to subtract.
+#'
+#' @param a A number.
+#' @param b It is another number again.
+#'
+#' @examples
+#' subtrair(2, 3)
+#'
+#' @export
+subtrair <- function(a, b){
+ a - b
+}
